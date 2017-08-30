0eb3c30ce4bd744457d2bd71385cf4118b501b24
Dora <dora@DN0a228734.SUNet>
2017-08-30 11:31:09 -0700

    added json schema for the MEG-BIDS _fid.json file

--- examples/plot_validator.py
+++ examples/plot_validator.py
@@ -1,9 +1,13 @@
 import os.path as op
 
 from mne_bids.validator import validate_meg
-from mne.datasets import sample
 
-data_path = sample.data_path()
+# test an _meg.json file:
 json_fname = op.join('.', 'sub-01_task-audiovisual_meg.json')
+
+validate_meg(json_fname)
+
+# test an _fid.json file:
+json_fname = op.join('.', 'sub-testme01_meg_fid.json')
 
 validate_meg(json_fname)
--- mne_bids/validator.py
+++ mne_bids/validator.py
@@ -9,10 +9,19 @@ base_dir = op.join(op.dirname(op.abspath(FILE)), 'jsonschemas')
 
 
 def validate_meg(fname):
-    with open(op.join(base_dir, 'schema_meg.json')) as json_data:
+    # get the correct schema
+    if fname.endswith('_meg.json'):
+        schema_fname = 'schema_meg.json'
+    elif fname.endswith('_fid.json'):
+        schema_fname = 'schema_meg_fid.json'
+
+    # open the schema
+    with open(op.join(base_dir, schema_fname)) as json_data:
         schema = json.load(json_data)
 
+    # open the BIDS json file to validate
     with open(fname) as json_data:
-        meg_json = json.load(json_data)
+        test_json = json.load(json_data)
 
-    validate(meg_json, schema)
+    # validate it
+    validate(test_json, schema)
