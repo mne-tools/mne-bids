4e2d1f2c8270a936b8bed698b7ae12a8785284d1
Teon L Brooks <teon.brooks@gmail.com>
2017-09-04 01:19:57 -0700

    update example

--- examples/plot_mne_sample.py
+++ examples/plot_mne_sample.py
@@ -10,22 +10,23 @@ your existing files into a BIDS-compatible folder.
 
 # Authors: Mainak Jas <mainak.jas@telecom-paristech.fr>
 #          Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
+#          Teon Brooks <teon.brooks@gmail.com>
 
 # License: BSD (3-clause)
 
 import os.path as op
 from mne.datasets import sample
-from mne_bids import folder_to_bids
+from mne_bids import raw_to_bids
 
 data_path = sample.data_path()
 event_id = {'Auditory/Left': 1, 'Auditory/Right': 2, 'Visual/Left': 3,
             'Visual/Right': 4, 'Smiley': 5, 'Button': 32}
 
-fnames = dict(events='sample_audvis_raw-eve.fif',
-              raw='sample_audvis_raw.fif')
-
-input_path = op.join(data_path, 'MEG', 'sample')
+input_fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw.fif')
+events_fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw-eve.fif')
 output_path = op.join(data_path, '..', 'MNE-sample-data-bids')
-folder_to_bids(input_path=input_path, output_path=output_path,
-               fnames=fnames, subject_id='01', run='01', task='audiovisual',
-               event_id=event_id, overwrite=True)
+
+raw_to_bids(subject_id='01', run='01', task='audiovisual',
+            input_fname=input_fname, output_path=output_path,
+            events_fname=events_fname, event_id=event_id,
+            overwrite=True)
