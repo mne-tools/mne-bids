be069c45d73692fbe01a674ec7ef0e836b698bf3
Mainak Jas <mainakjas@gmail.com>
2018-04-24 10:22:09 +0200

    Make install

--- _downloads/convert_mne_sample.py
+++ _downloads/convert_mne_sample.py
@@ -1,7 +1,7 @@
 """
-===================================
-Create a BIDS-compatible MEG folder
-===================================
+============================================
+Convert Raw data to a BIDS-compatible folder
+============================================
 
 Brain Imaging Data Structure (BIDS) MEG is a new standard for
 storing MEG files. This example demonstrates how to convert
@@ -28,14 +28,14 @@ data_path = sample.data_path()
 event_id = {'Auditory/Left': 1, 'Auditory/Right': 2, 'Visual/Left': 3,
             'Visual/Right': 4, 'Smiley': 5, 'Button': 32}
 
-raw_fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw.fif')
-events_fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw-eve.fif')
+raw_file = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw.fif')
+events_data = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw-eve.fif')
 output_path = op.join(data_path, '..', 'MNE-sample-data-bids')
 
 ###############################################################################
-# Finally, we specify the raw_fname and events_fname
+# Finally, we specify the raw_file and events_data
 
-raw_to_bids(subject_id='01', run='01', task='audiovisual',
-            raw_fname=raw_fname, events_fname=events_fname,
+raw_to_bids(subject_id='01', run='01', session_id='01', task='audiovisual',
+            raw_file=raw_file, events_data=events_data,
             output_path=output_path, event_id=event_id,
             overwrite=True)
