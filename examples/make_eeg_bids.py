8425d78649c2acd360386ae0d1c9b6635f043256
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-07-27 11:18:52 +0200

    not using _TempDir() anymore. Removed codeblock

--- examples/make_eeg_bids.py
+++ examples/make_eeg_bids.py
@@ -15,12 +15,12 @@ data. Specifically, we will follow these steps:
 # Authors: Stefan Appelhoff <stefan.appelhoff@mailbox.org>
 # License: BSD (3-clause)
 
+###############################################################################
 # We are importing everything we need for this example:
 import os
 
 from mne import find_events
 from mne.io import read_raw_brainvision
-from mne.utils import _TempDir
 
 from mne_bids import raw_to_bids
 from mne_bids.datasets import download_matchingpennies_subj
@@ -36,8 +36,11 @@ from mne_bids.utils import print_dir_tree
 #
 # For convenience, MNE-Bids has a function to download this data.
 
-# make a temporary directory to save the data to
-data_dir = _TempDir()
+# make a directory to save the data to
+home = os.path.expanduser('~')
+data_dir = os.path.join(home, 'mne_data', 'eeg_matchingpennies')
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
 
 ###############################################################################
 # Now, we download data for participant sub-05 (about 350MB). We could also
@@ -48,15 +51,19 @@ download_matchingpennies_subj(directory=data_dir)
 
 ###############################################################################
 # Let's see whether the data has been downloaded using a quick visualization
+# of the directory tree.
 print_dir_tree(data_dir)
 
 ###############################################################################
-# The data are part of the example suit for the EEG specification of BIDS, so
+# The data are part of the example suite for the EEG specification of BIDS, so
 # they are already in BIDS format as we can see from the file names (also, see
 # the `channels.tsv` and `events.tsv` files). However, we want to start from
 # scratch and will only use the "pure" EEG data in its BrainVision format with
-# a '.eeg' for binary data, a '.vhdr' for header information, and a '.vmrk' for
-# specifying the event markers within the data.
+# the following files:
+#
+# * '.eeg' for binary data
+# * '.vhdr' for header information
+# * '.vmrk' for specifying the event markers within the data.
 
 ###############################################################################
 # Step 2: Formatting as BIDS
@@ -73,7 +80,7 @@ print(raw)
 # the events of the EEG recording are stored. It has automatically created a
 # new "stimulus channel" containing the event pulses and appended this to our
 # data:
-print(raw.ch_names)  # Should be called something like "STI"
+print(raw.ch_names)  # There should be a channel called 'STI 014'
 
 ###############################################################################
 # For BIDS, we specify all events in a dedicated `events.tsv` file. Let's read
@@ -87,30 +94,16 @@ events = find_events(raw)
 # as possible from the raw data and then format it in a BIDS compatible way.
 # `raw_to_bids` takes a bunch of inputs, most of which are however optional.
 # The required inputs are described in the docstring:
-#
-# .. code-block:: python
-#    """
-#    Walk over a folder of files and create BIDS compatible folder.
-#
-#    Parameters
-#    ----------
-#    subject_id : str
-#        The subject name in BIDS compatible format ('01', '02', etc.)
-#    task : str
-#        Name of the task the data is based on.
-#    raw_file : str | instance of mne.Raw
-#        The raw data. If a string, it is assumed to be the path to the raw
-#        data file. Otherwise it must be an instance of mne.Raw
-#    output_path : str
-#        The path of the BIDS compatible folder
-#     """
+help(raw_to_bids)
+
+###############################################################################
 # Let's assume the previous file name `sub-05_task-matchingpennies_eeg`
 # contained an error and that this was actually `sub-01` and we also want to
 # have another task name `MatchingPennies`.
 subject_id = '01'
 task = 'MatchingPennies'
 raw_file = raw  # We can specify the raw file, or the path to the .vhdr file.
-output_path = _TempDir()
+output_path = os.path.join(home, 'mne_data', 'eeg_matchingpennies_bids_tmp')
 
 ###############################################################################
 # Now we just need to specify a few more EEG details to get something sensible:
@@ -144,9 +137,9 @@ raw_to_bids(subject_id=subject_id, task=task, raw_file=raw_file,
 #    ---------- README
 #    ---------- sub-xx
 #        ---------- modality
-#            ---------- sub-xx_task-yy_events.tsv
 #            ---------- sub-xx_task-yy_modality.ext
 #            ---------- sub-xx_task-yy_modality.json
+#            ---------- sub-xx_task-yy_events.tsv
 #
 #
 # Now, what does it actually look like?
