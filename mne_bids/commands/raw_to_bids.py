a2157f147281634b94c0da00cbb8483a9eb2a89e
Teon L Brooks <teon.brooks@gmail.com>
2018-08-08 13:50:44 -0400

    cleanup for the cli

--- mne_bids/commands/raw_to_bids.py
+++ mne_bids/commands/raw_to_bids.py
@@ -3,7 +3,7 @@
 
 """Command line interface for meg_bids.
 
-example usage:  $ mne_bids raw_to_bids --subject sub01 --task rest --out out_path
+example usage:  $ mne_bids raw_to_bids --subject_id sub01 --task rest --raw_file data.edf --output_path new_path
 
 """
 
@@ -17,28 +17,30 @@ def run():
 
     parser = get_optparser(__file__)
 
-    parser.add_option('--subject', dest='subject_id',
+    parser.add_option('--subject_id', dest='subject_id',
                       help='The subject name in BIDS compatible format (01, 02, etc.)', metavar='sub')
-    parser.add_option('--session', dest='session_id',
+    parser.add_option('--task', dest='task',
+                      help='Name of the task the data is based on.', metavar='t')
+    parser.add_option('--raw_file', dest='raw_file',
+                      help='The path to the raw MEG file.', metavar='raw')
+    parser.add_option('--output_path', dest='output_path',
+                      help='The path of the BIDS compatible folder.', metavar='o')
+    parser.add_option('--session_id', dest='session_id',
                       help='The session name in BIDS compatible format.', metavar='ses')
     parser.add_option('--run', dest='run',
-                      help='The run number in BIDS compatible format.', metavar='r')
-    parser.add_option('--task', dest='task',
-                      help='The task name.', metavar='t')
-    parser.add_option('--raw', dest='raw_fname',
-                      help='The path to the raw MEG file.', metavar='raw')
-    parser.add_option('--out', dest='out_path',
-                      help='The path of the BIDS compatible folder.', metavar='o')
-    parser.add_option('--events', dest='events_fname',
-                      help='The path to the events file.', metavar='evt')
+                      help='The run number for this dataset.', metavar='r')
+    parser.add_option('--kind', dest='kind',
+                      help='The kind of data being converted.', metavar='k')
+    parser.add_option('--events_data', dest='events_data',
+                      help='The events file.', metavar='evt')
     parser.add_option('--event_id', dest='event_id',
-                      help='The path to the event id file.', metavar='eid')
+                      help='The event id dict.', metavar='eid')
     parser.add_option('--hpi', dest='hpi',
                       help='The path to the MEG Marker points', metavar='filename')
     parser.add_option('--electrode', dest='electrode',
                       help='The path to head-native Digitizer points', metavar='elec')
     parser.add_option('--hsp', dest='hsp',
-                      help='The path to headshape points', metavar='hsp')
+                      help='The path to headshape points.', metavar='hsp')
     parser.add_option('--config', dest='config',
                       help='The path to the configuration file', metavar='cfg')
     parser.add_option('--overwrite', dest='overwrite',
@@ -47,8 +49,8 @@ def run():
 
     opt, args = parser.parse_args()
 
-    raw = raw_to_bids(opt.subject_id, opt.session_id, opt.run, opt.task,
-                      opt.raw_fname, opt.output_path, opt.events_fname,
+    raw = raw_to_bids(opt.subject_id, opt.task, opt.raw_file, opt.output_path, opt.session_id,
+                      opt.run, opt.kind, opt.events_data,
                       opt.event_id, opt.hpi, opt.electrode, opt.hsp,
                       opt.config, opt.overwrite, verbose=True)
 
