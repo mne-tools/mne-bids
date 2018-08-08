6312557c437ef2413647d0c83eabe876f0d6b8d2
Teon L Brooks <teon.brooks@gmail.com>
2018-08-08 18:05:48 -0400

    add tests

---
+++ mne_bids/tests/test_commands.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from os import path as op
+
+import mne
+from mne.datasets import testing
+from mne.utils import _TempDir, run_tests_if_main
+from mne_bids.commands import mne_bids_raw_to_bids
+
+
+subject_id = '01'
+task = 'testing'
+
+
+def check_usage(module, force_help=False):
+    """Ensure we print usage."""
+    args = ('--help',) if force_help else ()
+    with ArgvSetter(args) as out:
+        try:
+            module.run()
+        except SystemExit:
+            pass
+        assert 'Usage: ' in out.stdout.getvalue()
+
+
+def test_raw_to_bids():
+    """Test mne_bids raw_to_bids."""
+    output_path = _TempDir()
+    data_path = testing.data_path()
+    raw_fname = op.join(data_path, 'MEG', 'sample',
+                        'sample_audvis_filt-0-40_raw.fif')
+    check_usage(mne_bids_raw_to_bids)
+    with ArgvSetter(('--subject_id', subject_id, '--task', task, '--raw',
+                     raw_fname, '--output_path', output_path)):
+        mne_bids_raw_to_bids.run()
+
+
+run_tests_if_main()
