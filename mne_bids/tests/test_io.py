c1ee0b73dc7db9b5cc64c460bb7b842212026810
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-11-09 22:15:57 +0100

    test ieeg code in io module

--- mne_bids/tests/test_io.py
+++ mne_bids/tests/test_io.py
@@ -2,7 +2,10 @@
 # Authors: Stefan Appelhoff <stefan.appelhoff@mailbox.org>
 #
 # License: BSD (3-clause)
+import os.path as op
 import pytest
+
+from mne.utils import _TempDir
 
 from mne_bids.io import _parse_ext, _read_raw
 
@@ -25,5 +28,17 @@ def test_read_raw():
     """Test the raw reading."""
     # Use a file ending that does not exist
     f = 'file.bogus'
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='file name extension must be one of'):
         _read_raw(f)
+
+
+def test_not_implemented():
+    """Test the not yet implemented data formats raise an adequate error."""
+    for not_implemented_ext in ['.mef', '.nwb']:
+        data_path = _TempDir()
+        raw_fname = op.join(data_path, 'test' + not_implemented_ext)
+        with open(raw_fname, 'w'):
+            pass
+        with pytest.raises(ValueError, match=('there is no IO support for '
+                                              'this file format yet')):
+            _read_raw(raw_fname)
