60eb1a1d92098c120087aa247b216bfc92bbffa5
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-02-25 18:54:10 +0100

    Allow adding non maxfiltered datafiles

--- mne_bids/commands/mne_bids_raw_to_bids.py
+++ mne_bids/commands/mne_bids_raw_to_bids.py
@@ -54,6 +54,9 @@ def run():
     parser.add_option('--overwrite', dest='overwrite',
                       help=("Boolean. Whether to overwrite existing data"),
                       metavar='ow')
+    parser.add_option('--allow_maxshield', dest='allow_maxshield',
+                      help=("Boolean. Whether to allow non Maxfiltered data."),
+                      metavar='mxf')
 
     opt, args = parser.parse_args()
 
@@ -61,7 +64,7 @@ def run():
         subject=opt.subject_id, session=opt.session_id, run=opt.run,
         acquisition=opt.acq, task=opt.task)
     raw = _read_raw(opt.raw_fname, hpi=opt.hpi, electrode=opt.electrode,
-                    hsp=opt.hsp, config=opt.config)
+                    hsp=opt.hsp, config=opt.config, allow_maxshield=opt.allow_maxshield)
     write_raw_bids(raw, bids_basename, opt.output_path, event_id=opt.event_id,
                    events_data=opt.events_data, overwrite=opt.overwrite,
                    verbose=True)
