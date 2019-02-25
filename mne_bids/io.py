964b946ef9994de0ae7a492602ba9b2601e62d79
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-02-25 22:24:54 +0100

    fix long line and default maxshield to False

--- mne_bids/io.py
+++ mne_bids/io.py
@@ -47,7 +47,7 @@ def _parse_ext(raw_fname, verbose=False):
 
 
 def _read_raw(raw_fname, electrode=None, hsp=None, hpi=None, config=None,
-              montage=None, verbose=None, allow_maxshield=None):
+              montage=None, verbose=None, allow_maxshield=False):
     """Read a raw file into MNE, making inferences based on extension."""
     fname, ext = _parse_ext(raw_fname)
 
