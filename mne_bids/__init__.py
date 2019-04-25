c09a0733855d73c007ff617ceca1d718f52c8f6c
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-04-25 18:29:51 +0200

    rename mne_bids --> write.py

--- mne_bids/__init__.py
+++ mne_bids/__init__.py
@@ -3,7 +3,7 @@
 __version__ = '0.2.dev0'
 
 
-from .mne_bids import write_raw_bids  # noqa
+from .write import write_raw_bids  # noqa
 from .read import read_raw_bids  # noqa
 from .utils import make_bids_folders, make_bids_basename  # noqa
 from .config import BIDS_VERSION  # noqa
