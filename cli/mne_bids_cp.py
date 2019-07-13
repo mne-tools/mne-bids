288a2d2b0b159499cde73b1d7e138c69856654d0
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-13 18:12:19 +0200

    start cli for cp files

---
+++ cli/mne_bids_cp.py
@@ -0,0 +1,7 @@
+"""Provide copyfile utilities to the command line.
+
+example usage: $ mne_bids cp --input myfile.vhdr --output sub-01_task-test.vhdr
+"""
+# Authors: Stefan Appelhoff <stefan.appelhoff@mailbox.org>
+#
+# License: BSD (3-clause)
