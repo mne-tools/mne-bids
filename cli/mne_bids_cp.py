f8401c96e1e4a4e4082fa258cd72fe773a4b4389
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-15 13:36:27 +0200

    flesh out cp cli functionality

--- cli/mne_bids_cp.py
+++ cli/mne_bids_cp.py
@@ -1,7 +1,56 @@
-"""Provide copyfile utilities to the command line.
+"""Rename files (making a copy) and update their internal pointers.
 
 example usage: $ mne_bids cp --input myfile.vhdr --output sub-01_task-test.vhdr
 """
 # Authors: Stefan Appelhoff <stefan.appelhoff@mailbox.org>
 #
 # License: BSD (3-clause)
+import mne_bids
+from mne_bids.copyfiles import (copyfile_brainvision, copyfile_eeglab,
+                                copyfile_ctf)
+
+
+def run():
+    """Run the cp command."""
+    from mne.commands.utils import get_optparser
+
+    accepted_formats_msg = ('Accepted formats are BrainVision (.vhdr), '
+                            'EEGLAB (.set), CTF (.ds).')
+
+    parser = get_optparser(__file__, usage="usage: %prog -i INPUT -o OUTPUT",
+                           prog='mne_bids', version=mne_bids.__version__)
+
+    parser.add_option('-i', '--input', dest='input',
+                      help=('Path to the input file. {}'
+                            .format(accepted_formats_msg)), metavar='INPUT')
+
+    parser.add_option('-o', '--output', dest='output',
+                      help=('Path to the output file. MUST be same format '
+                            'as input file.'), metavar='OUTPUT')
+
+    opt, args = parser.parse_args()
+    opt_dict = vars(opt)
+
+    # Check the usage and raise error if invalid
+    if len(args) > 0:
+        parser.error('Do not specify additional arguments. Found: "{}"'
+                     .format(args))
+
+    if not opt_dict.get('input') or not opt_dict.get('output'):
+        parser.error('Incorrect number of arguments. Supply one input and one '
+                     'output file. You supplied: "{}"'.format(opt))
+
+    # Attempt to do the copying. Errors will be raised by the copyfile
+    # functions if there are issues with the file formats
+    if opt.input.endswith('.vhdr'):
+        copyfile_brainvision(opt.input, opt.output)
+    elif opt.input.endswith('.set'):
+        copyfile_eeglab(opt.input, opt.output)
+    elif opt.input.endswith('.ds'):
+        copyfile_ctf(opt.input, opt.output)
+    else:
+        parser.error('{} You supplied: "{}"'.format(accepted_formats_msg, opt))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
