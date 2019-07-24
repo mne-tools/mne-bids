0effdb96f99bc5441d0c4100417e4b1387b94e55
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-24 12:20:04 +0200

    improve tests

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -5,6 +5,7 @@
 # License: BSD (3-clause)
 from os import path as op
 
+import pytest
 from mne.datasets import testing
 from mne.utils import _TempDir, run_tests_if_main, ArgvSetter
 
@@ -34,10 +35,18 @@ def test_raw_to_bids():
     data_path = testing.data_path()
     raw_fname = op.join(data_path, 'MEG', 'sample',
                         'sample_audvis_trunc_raw.fif')
-    # check_usage(mne_bids_raw_to_bids)
+    # Check that help is printed
+    check_usage(mne_bids_raw_to_bids)
+
+    # Should work
     with ArgvSetter(('--subject_id', subject_id, '--task', task, '--raw',
                      raw_fname, '--output_path', output_path)):
         mne_bids_raw_to_bids.run()
+
+    # Too few input args
+    with pytest.raises(SystemExit):
+        with ArgvSetter(('--subject_id', subject_id)):
+            mne_bids_cp.run()
 
 
 def test_cp():
@@ -46,9 +55,18 @@ def test_cp():
     data_path = fetch_brainvision_testing_data()
     raw_fname = op.join(data_path, 'test.vhdr')
     outname = op.join(output_path, 'test2.vhdr')
-    # check_usage(mne_bids_cp)
+
+    # Check that help is printed
+    check_usage(mne_bids_cp)
+
+    # should work
     with ArgvSetter(('--input', raw_fname, '--output', outname)):
         mne_bids_cp.run()
+
+    # too few inputs
+    with pytest.raises(SystemExit):
+        with ArgvSetter(('--input', raw_fname)):
+            mne_bids_cp.run()
 
 
 run_tests_if_main()
