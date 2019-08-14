cc131ceaffe1a8ad2cb71492cbfbee00ded79ebd
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-08-14 22:21:35 +0200

    [MRG] remove reliance on datasets.py module (#254)
    * try nellh fix for windows bids validator
    
    * try new fix by @nellh
    
    * go back to bids-validator master: fixed for Windows
    
    * awscli dependency
    
    * do not use datasets.py function in cli test
    
    * use awscli in example
    
    * fix typo
    
    * add awscli as optional dependency in README
    
    * use awscli in convert group studies example
    
    * do not use awscli in renaming example
    
    * group studies example without awscli
    
    * remove mention of awscli
    
    * fix warning directive
    
    * revers ds000117 example changes
    
    * fix warning directive
    
    * fix the warning directive finally

--- examples/rename_brainvision_files.py
+++ examples/rename_brainvision_files.py
@@ -30,36 +30,42 @@ for the :ref:`Python Command Line Interface <python_cli>`.
 
 ###############################################################################
 # We are importing everything we need for this example:
-import os
+import os.path as op
 
 from numpy.testing import assert_array_equal
-from mne.io import read_raw_brainvision
+import mne
 
-from mne_bids.datasets import fetch_brainvision_testing_data
 from mne_bids.copyfiles import copyfile_brainvision
 
 ###############################################################################
 # Step 1: Download some example data
 # ----------------------------------
 # To demonstrate the MNE-BIDS functions, we need some testing data. Here, we
-# will use data that is shipped with MNE-Python. Feel free to use your own
-# BrainVision data.
-examples_dir = fetch_brainvision_testing_data()
+# will use the MNE-Python testing data. Feel free to use your own BrainVision
+# data.
+#
+# .. warning:: This will download 1.6 GB of data!
+
+data_path = mne.datasets.testing.data_path()
+examples_dir = op.join(data_path, 'Brainvision')
 
 ###############################################################################
 # Step 2: Rename the recording
 # ----------------------------------
-# Above, at the top of the example, we imported ``copyfile_brainvision`` from
+# Above, at the top of the example, we imported
+# :func:`mne_bids.utils.copyfile_brainvision` from
 # the MNE-BIDS ``utils.py`` module. This function takes two arguments as
 # input: First, the path to the existing .vhdr file. And second, the path to
 # the future .vhdr file.
 #
-# ``copyfile_brainvision`` will then create three new files (.vhdr, .vmrk, and
-# .eeg) with the new names as provided with the second argument.
+# :func:`mne_bids.utils.copyfile_brainvision` will then create three new files
+# (.vhdr, .vmrk, and .eeg) with the new names as provided with the second
+# argument.
 #
-# Here, we rename test.vhdr to test_renamed.vhdr:
-vhdr_file = os.path.join(examples_dir, 'test.vhdr')
-vhdr_file_renamed = os.path.join(examples_dir, 'test_renamed.vhdr')
+# Here, we rename a test file name:
+
+vhdr_file = op.join(examples_dir, 'test_NO.vhdr')
+vhdr_file_renamed = op.join(examples_dir, 'test_renamed.vhdr')
 copyfile_brainvision(vhdr_file, vhdr_file_renamed)
 
 ###############################################################################
@@ -67,8 +73,8 @@ copyfile_brainvision(vhdr_file, vhdr_file_renamed)
 # --------------------------------------------------------------
 # Finally, let's use MNE-Python to read in both, the original BrainVision data
 # as well as the renamed data. They should be the same.
-raw = read_raw_brainvision(vhdr_file)
-raw_renamed = read_raw_brainvision(vhdr_file_renamed)
+raw = mne.io.read_raw_brainvision(vhdr_file)
+raw_renamed = mne.io.read_raw_brainvision(vhdr_file_renamed)
 
 assert_array_equal(raw.get_data(), raw_renamed.get_data())
 
