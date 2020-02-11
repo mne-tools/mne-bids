bc98ee9fbd3dad87db9f7c652b22e302e4a3377b
Adam Li <adam2392@gmail.com>
2020-02-10 23:46:52 -0500

    Wrapping unit test in mne version.

--- mne_bids/tests/test_write.py
+++ mne_bids/tests/test_write.py
@@ -690,12 +690,13 @@ def test_edf(_bids_validate):
     assert len(list(data.values())[0]) == 2
 
     # check that scans list is properly converted to brainvision
-    bids_root = write_raw_bids(raw, bids_basename, bids_root,
-                               anonymize=dict(daysback=33000),
-                               overwrite=True)
-    data = _from_tsv(scans_tsv)
-    bids_fname = bids_basename + "_eeg.vhdr"
-    assert any([bids_fname in fname for fname in data['filename']])
+    if check_version('mne', '0.20') and check_version('pybv', '0.2.0'):
+        write_raw_bids(raw, bids_basename, bids_root,
+                                   anonymize=dict(daysback=33000),
+                                   overwrite=True)
+        data = _from_tsv(scans_tsv)
+        bids_fname = bids_basename + "_eeg.vhdr"
+        assert any([bids_fname in fname for fname in data['filename']])
 
     # Also cover iEEG
     # We use the same data and pretend that eeg channels are ecog
