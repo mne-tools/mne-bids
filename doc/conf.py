55cccda28b49a7f886e9f71db14fbf17cb786bfc
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-03-24 12:44:41 +0100

    re-add templates path

--- doc/conf.py
+++ doc/conf.py
@@ -48,6 +48,9 @@ extensions = [
 # generate autosummary even if no references
 autosummary_generate = True
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
