e4b28563842537d7a022f63c6f2c49f837b63c0e
kalenkovich <e.kalenkovich@gmail.com>
2020-06-21 18:31:50 +0300

    added `@verbose` decorator to `utils._read_events`

--- mne_bids/utils.py
+++ mne_bids/utils.py
@@ -20,7 +20,7 @@ from copy import deepcopy
 
 import numpy as np
 from mne import read_events, find_events, events_from_annotations
-from mne.utils import check_version, warn, logger
+from mne.utils import check_version, warn, logger, verbose
 from mne.channels import make_standard_montage
 from mne.io.pick import pick_types
 from mne.io.kit.kit import get_kit_info
@@ -710,6 +710,7 @@ def _check_key_val(key, val):
     return key, val
 
 
+@verbose
 def _read_events(events_data, event_id, raw, ext, verbose=False):
     """Read in events data.
 
