cd6b9f014576eaa7d18697c5187db20839af021f
Adam Li <adam2392@gmail.com>
2020-06-22 14:40:56 -0400

    Adding report.

--- mne_bids/report.py
+++ mne_bids/report.py
@@ -4,101 +4,183 @@
 # License: BSD (3-clause)
 import collections
 import json
-from pathlib import Path
 import os
-from pprint import pprint
+from pathlib import Path
 
-from mne_bids import make_bids_basename
-from mne_bids.utils import get_kinds, get_entity_vals, _find_matching_sidecar
+import numpy as np
+
+from mne_bids.datasets import fetch_brainvision_testing_data
 from mne_bids.tsv_handler import _from_tsv
-
-PARTICIPANTS_TEMPLATE = "The dataset consists of {n_patients} patients " \
-                        "({patient_count_summary})."
-SCAN_TEMPLATE = "The session {session} consists of {n_scans} " \
-                "with {scan_summary}."
-DATASET_TEMPLATE = "The data was acquired using a {{system}} system ({{manufacturer}}) " \
-                   "consisting of {{channel_summary}}."
+from mne_bids.utils import (make_bids_basename, get_kinds,
+                            get_entity_vals, _parse_ext)
 
 
-def _create_subject_tree(bids_root, verbose=True):
-    # create a tree for the patient summaries in the form of a nested dictionary
-    patient_summaries = dict()
+def _summarize_subs(bids_root) -> str:
+    """Summarize subjects in BIDS root directory.
 
-    # loop through each subject
-    subjects = get_entity_vals(bids_root, entity_key='sub')
+    Parameters
+    ----------
+    bids_root : str | pathlib.Path
 
-    for subject in subjects:
-        ignore_sub = [sub for sub in subjects if sub != subject]
-        sessions = get_entity_vals(bids_root, entity_key='ses', ignore_sub=ignore_sub)
-        tasks = get_entity_vals(bids_root, entity_key='task', ignore_sub=ignore_sub)
-        acquisitions = get_entity_vals(bids_root, entity_key='acq', ignore_sub=ignore_sub)
+    Returns
+    -------
+    template : str
+    """
+    participants_tsv_fpath = make_bids_basename(prefix=bids_root,
+                                                suffix='participants.tsv')
+    participants_tsv = _from_tsv(participants_tsv_fpath)
+    n_subjects = len(participants_tsv['participant_id'])
 
-        # if subject not in patient_summaries:
-        #     patient_summaries[subject] = dict()
-        # patient_summaries[subject]['ses'] = sessions
+    # summarize sex
+    sex_str = ""
+    count = 0
+    keys = {'M': 'male', 'F': 'female', 'n/a': 'unknown sex'}
+    for key, str_val in keys.items():
+        n_sex_ct = len([sex for sex in participants_tsv['sex'] if sex == key])
+        if n_sex_ct > 0:
+            sex_str = sex_str + f'{n_sex_ct} {str_val} '
+            count += 1
 
-        for session in sessions:
-            # if session not in patient_summaries[subject]['ses']:
-            scans_fpath = make_bids_basename(subject, session, suffix='scans.tsv')
-            scans_tsv = _from_tsv(scans_fpath, verbose)
-            scans = scans_tsv['filename']
+    # summarize hand
+    hand_str = ""
+    count = 0
+    keys = {'R': 'right', 'L': 'left',
+            'A': 'ambidextrous', 'n/a': 'unknown hand'}
+    for key, str_val in keys.items():
+        n_hand_ct = len([hand for hand in participants_tsv['hand']
+                         if hand == key])
+        if n_hand_ct > 0:
+            hand_str = hand_str + f'{n_hand_ct} {str_val} '
+            count += 1
 
-            # for scan in scans:
+    # summarize age
+    age_list = [float(age) for age in participants_tsv['age'] if age != 'n/a']
+    n_unknown = n_subjects - len(age_list)
+    age_str = 'ages '
+    if age_list:
+        avg_age, std_age = np.mean(age_list), np.std(age_list)
+        age_str = f'{avg_age} +/- {std_age}'
+    if n_unknown:
+        age_str = age_str + f' with {n_unknown} unknown age'
 
-            # patient_summaries[subject][session]['task'] = tasks
-            # patient_summaries[subject][session]['acq'] = acquisitions
-            # patient_summaries[subject][session]['scans'] = scans
-
-def create_methods_paragraph(bids_root, verbose=True):
-    # summarizing statistics
-    n_kinds = 0
-    kinds = get_kinds(bids_root)
-    n_kinds += len(kinds)
-    n_sessions = 0
-    n_patients = 0
+    template = f'The dataset consists of {n_subjects} subjects ' \
+               f'({age_str}; {hand_str}; {sex_str}).'
+    return template
 
 
+def _summarize_scans(bids_root, session=None, kind=None) -> str:
+    """Summarize scans in BIDS root directory.
 
-    # loop through each subject
+    Parameters
+    ----------
+    bids_root : str | pathlib.Path
+    session : str, optional
+    kind : str, optional
+
+    Returns
+    -------
+    template : str
+    """
+    bids_root = Path(bids_root)
+    if session is None:
+        search_str = '*_scans.tsv'
+    else:
+        search_str = f'*ses-{session}*_scans.tsv'
+    scans_fpaths = list(bids_root.rglob(search_str))
+
+    if len(scans_fpaths) == 0:
+        return ''
+
+    # keep track of number of files, length and sfreq
+    n_files = 0
+    length_recordings = []
+    sampling_frequency = collections.defaultdict(int)
+
+    # loop through each scan
+    for scan_fpath in scans_fpaths:
+        # get the current session directory
+        ses_path = os.path.dirname(scan_fpath)
+
+        # load in the scans.tsv file
+        # and read metadata for each scan
+        scans_tsv = _from_tsv(scan_fpath)
+        scans = scans_tsv['filename']
+        for scan in scans:
+            if kind is not None:
+                if not scan.startswith(kind):
+                    continue
+
+            # count each scan
+            n_files += 1
+
+            # summarize metadata of recordings
+            bids_fname, _ = _parse_ext(scan)
+            sidecar_fname = os.path.join(ses_path, bids_fname + '.json')
+            with open(sidecar_fname, 'r') as fin:
+                sidecar_json = json.load(fin)
+
+            # aggregate metadata from each scan
+            length_recordings.append(sidecar_json['RecordingDuration'])
+            sfreq = np.round(sidecar_json['SamplingFrequency'])
+            sampling_frequency[sfreq] += 1
+
+    avg_length = np.mean(length_recordings)
+    std_length = np.std(length_recordings)
+    length_summary = f'{avg_length:.2f} +/- {std_length:.2f} seconds'
+
+    sfreq_summary = ''
+    for idx, (sfreq, count) in enumerate(sampling_frequency.items()):
+        if sfreq_summary == '':
+            sfreq_summary = 'with sampling rates '
+        if idx > 0:
+            sfreq_summary = sfreq_summary + ', '
+        sfreq_summary = sfreq_summary + f'{sfreq} (n={count})'
+
+    template = f'There are {n_files} datasets ({length_summary}) ' \
+               f'{sfreq_summary}.'
+    return template
+
+
+def create_methods_paragraph(bids_root, session=None, verbose=True):
+    """Create a methods paragraph string from BIDS dataset.
+
+    Parameters
+    ----------
+    bids_root : str | pathlib.Path
+    session : str , optional
+    verbose : bool
+
+    Returns
+    -------
+
+    """
+    # high level summary
     subjects = get_entity_vals(bids_root, entity_key='sub')
     sessions = get_entity_vals(bids_root, entity_key='ses')
+    kinds = get_kinds(bids_root)
 
-    for subject in subjects:
-        ignore_sub = [sub for sub in subjects if sub != subject]
-        sessions = get_entity_vals(bids_root, entity_key='ses', ignore_sub=ignore_sub)
-        tasks = get_entity_vals(bids_root, entity_key='task', ignore_sub=ignore_sub)
-        acquisitions = get_entity_vals(bids_root, entity_key='acq', ignore_sub=ignore_sub)
+    # summarizing statistics
+    n_patients, n_sessions = len(subjects), len(sessions)
+    n_kinds = len(kinds)
 
-        patient_summaries[subject]['ses'] = sessions
-
-        for session in sessions:
-            scans_fpath = make_bids_basename(subject, session, suffix='scans.tsv')
-            scans_tsv = _from_tsv(scans_fpath, verbose)
-            scans = scans_tsv['filename']
-
-            patient_summaries[subject][session]['task'] = tasks
-            patient_summaries[subject][session]['acq'] = acquisitions
-            patient_summaries[subject][session]['scans'] = scans
-
+    # template string for high level summary
+    session_summary = ', '.join(sessions)
     kind_summary = ', '.join(kinds)
-    PARTICIPANTS_TEMPLATE = f"The dataset consists of {n_patients} patients " \
-                            f"with {n_sessions} sessions consisting of {kind_summary} data."
+    template = f"The dataset consists of {n_patients} patients " \
+               f"with {n_sessions} sessions ({session_summary}) " \
+               f"consisting of {n_kinds} kinds of data ({kind_summary})."
 
-    paragraph = PARTICIPANTS_TEMPLATE
+    # subject and scans summary
+    sub_template = _summarize_subs(bids_root)
+    scan_template = _summarize_scans(bids_root, session=session)
+
+    paragraph = f'{template} {sub_template} {scan_template}'
     return paragraph
 
-def _scan_summary(bids_basename, bids_root, kind):
-    # XXX: to improve with separate summary dictionary for each kind
-    scan_dict = dict()
 
-    sidecar_fpath = _find_matching_sidecar(bids_basename, bids_root,
-                                           suffix=f'{kind}.json')
-    with open(sidecar_fpath, 'r') as fin:
-        sidecarjson = json.load(fin)
+if __name__ == '__main__':
+    bids_root = fetch_brainvision_testing_data()
+    bids_root = "/Users/adam2392/Downloads/tngpipeline"
 
-    scan_dict['RecordingDuration'] = sidecarjson['RecordingDuration']
-    scan_dict['PowerLineFrequeency'] = sidecarjson['PowerLineFrequency']
-    scan_dict['SamplingFrequency'] = sidecarjson['SamplingFrequency']
-    scan_dict['Manufacturer'] = sidecarjson['Manufacturer']
-
-    return scan_dict
+    methods_paragraph = create_methods_paragraph(bids_root)
+    print(methods_paragraph)
---
+++ mne_bids/tests/test_report.py
@@ -0,0 +1,60 @@
+"""Testing automatic BIDS report."""
+# Authors: Adam Li <adam2392@gmail.com>
+#
+# License: BSD (3-clause)
+import os.path as op
+import warnings
+
+import pytest
+
+with warnings.catch_warnings():
+    warnings.filterwarnings(action='ignore',
+                            message="can't resolve package",
+                            category=ImportWarning)
+    import mne
+
+from mne.utils import _TempDir
+from mne.datasets import testing
+
+from mne_bids import (make_bids_basename,
+                      create_methods_paragraph)
+from mne_bids.write import write_raw_bids
+
+subject_id = '01'
+session_id = '01'
+run = '01'
+acq = '01'
+task = 'testing'
+
+bids_basename = make_bids_basename(
+    subject=subject_id, session=session_id, run=run, acquisition=acq,
+    task=task)
+
+# Get the MNE testing sample data
+data_path = testing.data_path()
+raw_fname = op.join(data_path, 'MEG', 'sample',
+                    'sample_audvis_trunc_raw.fif')
+
+warning_str = dict(
+    channel_unit_changed='ignore:The unit for chann*.:RuntimeWarning:mne',
+)
+
+
+@pytest.mark.filterwarnings(warning_str['channel_unit_changed'])
+def test_read_raw_kind():
+    """Test that read_raw_bids() can infer the kind if need be."""
+    bids_root = _TempDir()
+    raw = mne.io.read_raw_fif(raw_fname, verbose=False)
+    write_raw_bids(raw, bids_basename, bids_root, overwrite=True,
+                   verbose=False)
+
+    methods_summ = create_methods_paragraph(bids_root)
+
+    assert methods_summ == 'The dataset consists of 1 patients ' \
+                           'with 1 sessions (01) ' \
+                           'consisting of 1 kinds of data (meg). ' \
+                           'The dataset consists of 1 subjects ' \
+                           '(ages  with 1 unknown age; ' \
+                           '1 unknown hand ; 1 unknown sex ). ' \
+                           'There are 1 datasets (20.00 +/- 0.00 seconds) ' \
+                           'with sampling rates 300.0 (n=1).'
