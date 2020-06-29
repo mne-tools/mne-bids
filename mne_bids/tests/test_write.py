0b49a1e9e9c41357398bab51c772b1e4d5a881d8
Alex <aprockhill206@gmail.com>
2020-06-28 17:22:40 -0700

    fix failed test

--- mne_bids/tests/test_write.py
+++ mne_bids/tests/test_write.py
@@ -759,7 +759,8 @@ def test_edf(_bids_validate):
     # after overwrite, the dataset description if defaulted to MNE-BIDS
     with open(dataset_description_fpath, 'r') as f:
         dataset_description_json = json.load(f)
-        assert dataset_description_json["Authors"] == ["MNE-BIDS"]
+        assert dataset_description_json["Authors"] == ["MNE-BIDS (Please cite "
+                                                       "before removing this)"]
 
     # Reading the file back should raise an error, because we renamed channels
     # in `raw` and used that information to write a channels.tsv. Yet, we
