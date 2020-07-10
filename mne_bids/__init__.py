bf4137753d1740bc4807ea9ce15610898b37b4ee
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-07-10 22:58:03 +0200

    fix __init__ mess created by isort

--- mne_bids/__init__.py
+++ mne_bids/__init__.py
@@ -1,11 +1,10 @@
 """MNE software for easily interacting with BIDS compatible datasets."""
 
 __version__ = '0.5.dev0'
-from mne_bids import commands  # noqa: F401
-from mne_bids.read import get_head_mri_trans, read_raw_bids
-from mne_bids.utils import (get_anonymization_daysback,  # noqa: F401
+from mne_bids import commands
+from mne_bids.read import (get_head_mri_trans, read_raw_bids,
+                           get_matched_empty_room)
+from mne_bids.utils import (get_anonymization_daysback,
                             make_bids_basename, make_bids_folders)
-from mne_bids.write import make_dataset_description, write_anat
-
-from mne_bids.read import get_matched_empty_room  # noqa: F401; noqa: F401
-from mne_bids.write import write_raw_bids  # noqa: F401; noqa: E501 F401
+from mne_bids.write import (make_dataset_description, write_anat,
+                            write_raw_bids)
