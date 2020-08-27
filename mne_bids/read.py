0fdf26eb391a9e9b0cfcc8f56e88d12b9e444106
Adam Li <adam2392@gmail.com>
2020-08-27 16:12:49 -0400

    Update mne_bids/read.py
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- mne_bids/read.py
+++ mne_bids/read.py
@@ -573,9 +573,9 @@ def get_head_mri_trans(bids_path):
     Parameters
     ----------
     bids_path : BIDSPath
-        The base filename of the BIDS compatible files. Typically, this can be
-        generated using :func:`mne_bids.BIDSPath`. The path to root of the
-        BIDS folder must be passed in via the ``BIDSPath`` object.
+        The path of the recording for which to retrieve the transformation. The
+        :class:`mne_bids.BIDSPath` instance passed here **must** have the
+        ``.root`` attribute set.
 
     Returns
     -------
