1196b600acbe5dcb00a7360035c8dfb2a34823cb
Adam Li <adam2392@gmail.com>
2020-10-02 12:50:20 -0400

    Adding additional info to stuff.

--- mne_bids/__init__.py
+++ mne_bids/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.5.dev0'
 from mne_bids import commands
 from mne_bids.report import make_report
 from mne_bids.path import (BIDSPath, get_datatypes, get_entity_vals,
-                           print_dir_tree)
+                           print_dir_tree, get_entities_from_fname)
 from mne_bids.read import get_head_mri_trans, read_raw_bids
 from mne_bids.utils import (get_anonymization_daysback)
 from mne_bids.write import (make_dataset_description, write_anat,
