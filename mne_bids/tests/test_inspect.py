c6f5ed913244c97b2e8b4bd21ed814ae2e8370ee
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-11-23 18:09:39 +0100

    WIP Actually do an inspection

--- mne_bids/tests/test_inspect.py
+++ mne_bids/tests/test_inspect.py
@@ -1,7 +1,54 @@
 """Test the interactive data inspector."""
 
-from mne_bids import inspect  # noqa
+import os.path as op
+import pytest
+
+import mne
+from mne.datasets import testing
+
+from mne_bids import BIDSPath, write_raw_bids, inspect
+
+from test_read import _read_raw_fif, warning_str
 
 
-def test_inspect():
-    pass
+subject_id = '01'
+session_id = '01'
+run = '01'
+acq = None
+task = 'testing'
+
+_bids_path = BIDSPath(
+    subject=subject_id, session=session_id, run=run, acquisition=acq,
+    task=task)
+
+
+@pytest.fixture(scope='session')
+def return_bids_test_dir(tmpdir_factory):
+    """Return path to a written test BIDS dir."""
+    bids_root = str(tmpdir_factory.mktemp('mnebids_utils_test_bids_ds'))
+    data_path = testing.data_path()
+    raw_fname = op.join(data_path, 'MEG', 'sample',
+                        'sample_audvis_trunc_raw.fif')
+
+    event_id = {'Auditory/Left': 1, 'Auditory/Right': 2, 'Visual/Left': 3,
+                'Visual/Right': 4, 'Smiley': 5, 'Button': 32}
+    events_fname = op.join(data_path, 'MEG', 'sample',
+                           'sample_audvis_trunc_raw-eve.fif')
+
+    raw = mne.io.read_raw_fif(raw_fname)
+    raw.info['line_freq'] = 60
+
+    # Drop unknown events.
+    events = mne.read_events(events_fname)
+    events = events[events[:, 2] != 0]
+
+    bids_path = _bids_path.copy().update(root=bids_root)
+    write_raw_bids(raw, bids_path=bids_path, events_data=events,
+                   event_id=event_id, overwrite=True)
+
+    return bids_root
+
+
+def test_inspect(return_bids_test_dir):
+    bids_path = _bids_path.copy()
+    inspect(bids_path)
