03f78e577c445509494d9bcf442fac064149db46
Ethan Knights <ethanknights@hotmail.co.uk>
2020-12-14 17:50:47 +0000

    [MRG] Edit 'no events found' warning for resting state data - Reopened (#631)
    * reapply old branch commit
    
    * change rest warning to logger.info
    
    * tiny fix
    
    * Use latest setup-node 1.x
    
    * warn
    
    * Update mne_bids/read.py
    
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>
    
    * add verbose check
    
    * rm verbose check
    
    * pep8
    
    * change task_rest to rest
    
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- mne_bids/read.py
+++ mne_bids/read.py
@@ -147,9 +147,11 @@ def _read_events(events_data, event_id, raw, verbose=None):
         verbose=verbose
     )
     all_dur = raw.annotations.duration
-    if all_events.size == 0:
-        warn('No events found or provided. Please add annotations to the raw '
-             'data, or provide the events_data and event_id parameters.')
+    if all_events.size == 0 and 'rest' not in raw.filenames[0]:
+        warn('No events found or provided. Please add annotations '
+             'to the raw data, or provide the events_data and '
+             'event_id parameters. If this is resting state data '
+             'it is recommended to name the task "rest".')
 
     return all_events, all_dur, all_desc
 
