8b5bd2abd391c5745e7483d7f4b8cdc298079ddb
Adam Li <adam2392@gmail.com>
2020-12-31 01:00:24 -0500

    Fixing read.

--- mne_bids/read.py
+++ mne_bids/read.py
@@ -211,6 +211,9 @@ def _handle_scans_reading(scans_fname, raw, bids_path, verbose=False):
     # extract the acquisition time from scans file
     acq_time = acq_times[row_ind]
     if acq_time != 'n/a':
+        if '.' not in acq_time:
+            # acquisition time ends with '.%fZ' microseconds string
+            acq_time += '.0Z'
         acq_time = datetime.strptime(acq_time, '%Y-%m-%dT%H:%M:%S.%fZ')
         acq_time = acq_time.replace(tzinfo=timezone.utc)
 
