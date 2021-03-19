0e044864506316bf057d9a3d607a39a05c4caca4
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-03-19 14:41:20 +0100

    Revert "use setup.cfg feature to get version"
    This reverts commit 492b227d9eecbea62018ab323a12ee170646c66c.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 """Setup MNE-BIDS."""
-import sys
-
+import os
 from setuptools import setup
 
-# Give setuptools a hint to complain if it's too old a version
-SETUP_REQUIRES = ["setuptools >= 46.4.0"]
-# This enables setuptools to install wheel on-the-fly
-SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
+# get the version
+version = None
+with open(os.path.join('mne_bids', '__init__.py'), 'r') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip('\'')
+            break
+if version is None:
+    raise RuntimeError('Could not determine version.')
 
 if __name__ == "__main__":
-    setup(
-        setup_requires=SETUP_REQUIRES,
-    )
+    setup(version=version)
