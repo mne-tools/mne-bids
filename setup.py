bfe57d003db8d857dd67080351d89812a927f403
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-03-19 12:08:27 +0100

    use setup.cfg feature to get version

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 """Setup MNE-BIDS."""
-import os
+import sys
+
 from setuptools import setup
 
-# get the version
-version = None
-with open(os.path.join('mne_bids', '__init__.py'), 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version.')
+# Give setuptools a hint to complain if it's too old a version
+SETUP_REQUIRES = ["setuptools >= 46.4.0"]
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
 if __name__ == "__main__":
-    setup(version=version)
+    setup(
+        setup_requires=SETUP_REQUIRES,
+    )
