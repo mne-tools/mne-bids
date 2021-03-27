f7ff7613cde3227feed936e601a52357746bb241
Alex <aprockhill@mailbox.org>
2021-03-27 08:29:24 -0700

    fix note formatting

--- examples/read_bids_datasets.py
+++ examples/read_bids_datasets.py
@@ -49,11 +49,11 @@ from mne_bids import BIDSPath, read_raw_bids, print_dir_tree, make_report
 # uses this `EEG dataset <https://openneuro.org/datasets/ds002778>`_ of
 # resting-state recordings of patients with Parkinson's disease.
 #
+
 # .. note: If the keyword argument include is left out of
-# ``openneuro.download``, the whole dataset will be downloaded.
-# We're just using data from one subject to reduce the time
-# it takes to run the example.
-#
+#          ``openneuro.download``, the whole dataset will be downloaded.
+#          We're just using data from one subject to reduce the time
+#          it takes to run the example.
 
 dataset = 'ds002778'
 subject = 'pd6'
