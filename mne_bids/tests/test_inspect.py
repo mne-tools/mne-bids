e105462a31c307576a5c6cabe5ebada8b8f73699
Alex <aprockhill@mailbox.org>
2021-03-27 19:26:08 -0700

    fix for mpl 3.4 requirements

--- mne_bids/tests/test_inspect.py
+++ mne_bids/tests/test_inspect.py
@@ -81,7 +81,7 @@ def test_inspect_single_file(tmp_path, save_changes):
     _click_ch_name(raw_fig, ch_index=4, button=1)
 
     # Closing the window should open a dialog box.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)  # manually for non-interactive backend
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
 
     if save_changes:
@@ -121,7 +121,7 @@ def test_inspect_multiple_files(tmp_path):
     inspect_dataset(bids_path.copy().update(subject=None))
     raw_fig = mne_bids.inspect._global_vars['raw_fig']
     assert raw_fig.mne.info['subject_info']['his_id'] == 'sub-02'
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
 
 
 @requires_matplotlib
@@ -148,7 +148,7 @@ def test_inspect_set_and_unset_bads(tmp_path):
     _click_ch_name(raw_fig, ch_index=4, button=1)
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -160,7 +160,7 @@ def test_inspect_set_and_unset_bads(tmp_path):
     _click_ch_name(raw_fig, ch_index=4, button=1)
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -209,7 +209,7 @@ def test_inspect_annotations(tmp_path):
     _add_annotation(raw_fig)
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -228,7 +228,7 @@ def test_inspect_annotations(tmp_path):
                 kind='press')
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -271,7 +271,7 @@ def test_inspect_annotations_remove_all(tmp_path):
     _add_annotation(raw_fig)
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -287,7 +287,7 @@ def test_inspect_annotations_remove_all(tmp_path):
                 kind='press')
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -338,7 +338,7 @@ def test_inspect_bads_and_annotations(tmp_path):
     _add_annotation(raw_fig)
 
     # Close window and save changes.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
     fig_dialog.canvas.key_press_event('return')
 
@@ -380,7 +380,7 @@ def test_inspect_auto_flats(tmp_path, save_changes):
     raw_fig = mne_bids.inspect._global_vars['raw_fig']
 
     # Closing the window should open a dialog box.
-    raw_fig.canvas.key_press_event(raw_fig.mne.close_key)
+    raw_fig._close(event=None)
     fig_dialog = mne_bids.inspect._global_vars['dialog_fig']
 
     if save_changes:
