f0c1daa21f7aa6fc30d27b63c6f02e38cc95a188
Adam Li <adam2392@gmail.com>
2021-03-31 11:25:17 -0400

    Adding note to docstring.

--- mne_bids/write.py
+++ mne_bids/write.py
@@ -1089,6 +1089,13 @@ def write_raw_bids(raw, bids_path, events_data=None,
     and the actual recording date will be stored in the ``scans.tsv``
     file's ``acq_time`` column.
 
+    ``write_raw_bids`` will generate a ``dataset_description.json`` file
+    if it does not already exist. Minimal metadata will be written there.
+    If one sets ``overwrite`` to ``True`` here, it will not overwrite an
+    existing ``dataset_description.json`` file.
+    If you need to add more data there, or overwrite it, then you should
+    call :func:`mne_bids.make_dataset_description` directly.
+
     See Also
     --------
     mne.io.Raw.anonymize
