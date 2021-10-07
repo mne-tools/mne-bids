47ab410c5db30b2fa50c689ea046a09f80a2b998
Adam Li <adam2392@gmail.com>
2021-10-06 20:35:10 -0400

    Try again

--- mne_bids/tests/conftest.py
+++ mne_bids/tests/conftest.py
@@ -31,18 +31,3 @@ def _bids_validate():
         run_subprocess(cmd, shell=shell)
 
     return _validate
-
-
-def pytest_configure(config):
-    """Configure pytest options."""
-    # Warnings
-    # - This list should also be considered alongside reset_warnings in
-    #   doc/conf.py.
-    warning_lines = r"""
-    error::
-    ignore:.*overwrite keyword.*:DeprecationWarning
-    """  # noqa: E501
-    for warning_line in warning_lines.split('\n'):
-        warning_line = warning_line.strip()
-        if warning_line and not warning_line.startswith('#'):
-            config.addinivalue_line('filterwarnings', warning_line)
