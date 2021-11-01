f2d1d52b910290959fa8af99d8cd6795fca16a21
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-11-01 11:27:57 +0100

    Log formatting

--- mne_bids/write.py
+++ mne_bids/write.py
@@ -2397,7 +2397,7 @@ def anonymize_dataset(bids_root_in, bids_root_out, daysback='auto',
         f'\n'
     )
     if daysback is None:
-        msg += 'Not shifting recording dates (found anatomical scans only).'
+        msg += 'Not shifting recording dates (found anatomical scans only).\n'
     else:
         msg += (
             f'Shifting recoding dates by {daysback} days '
