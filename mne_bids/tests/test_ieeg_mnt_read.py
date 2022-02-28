1986d0b0da9daa808f8ebec829ba1cd6adda37d5
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2022-02-28 23:00:32 +0100

    simplify testing

--- mne_bids/tests/test_ieeg_mnt_read.py
+++ mne_bids/tests/test_ieeg_mnt_read.py
@@ -1,11 +1,23 @@
-import os
+from pathlib import Path
 import mne_bids
+
 
 def test_read_ieeg_bids_data():
 
-    bids_path = mne_bids.BIDSPath(subject='01', session='01',
-                     task='audiovisual', run='01', root=os.path.join("data", "ieeg_bids"))
+    bids_path = mne_bids.BIDSPath(
+        subject="01",
+        session="01",
+        task="audiovisual",
+        run="01",
+        root=Path(__file__).parent / "data" / "ieeg_bids",
+    )
 
     raw_read = mne_bids.read_raw_bids(bids_path)
 
-    assert raw_read.get_montage().get_positions()["coord_frame"] == "MNI152NLin2009bAsym"
+    assert (
+        raw_read.get_montage().get_positions()["coord_frame"] == "MNI152NLin2009bAsym"
+    )
+
+
+if __name__ == '__main__':
+    test_read_ieeg_bids_data()
