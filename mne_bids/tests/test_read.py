e9b80d00c81338ab342f1446b48f9a6ca96109a7
timonmerk <timon.merk@charite.de>
2022-08-15 09:33:52 +0200

    fix import

--- mne_bids/tests/test_read.py
+++ mne_bids/tests/test_read.py
@@ -1245,14 +1245,14 @@ def test_file_not_found(tmp_path):
 
 def test_read_ieeg_coord_frame():
     """Ensure that the iEEG coordinate frame is read correctly."""
-    bids_path = mne_bids.BIDSPath(
+    bids_path = BIDSPath(
         subject="01",
         session="01",
         task="audiovisual",
         run="01",
         root=Path(__file__).parent / "data" / "ieeg_bids",
     )
-    raw_read = mne_bids.read_raw_bids(bids_path)
+    raw_read = read_raw_bids(bids_path)
 
     coord_frame = raw_read.get_montage().get_positions()["coord_frame"]
     assert coord_frame == "MNI152NLin2009bAsym"
