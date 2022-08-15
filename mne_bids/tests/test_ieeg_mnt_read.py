c4aac22158db045437e5ac6637e6e0964b2e09c4
timonmerk <38216460+timonmerk@users.noreply.github.com>
2022-08-15 09:18:37 +0200

    Apply suggestions from code review
    review test file changes
    
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- mne_bids/tests/test_ieeg_mnt_read.py
+++ mne_bids/tests/test_ieeg_mnt_read.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import mne_bids
 
 
-def test_read_ieeg_bids_data():
-
+def test_read_ieeg_coord_frame():
+"""Ensure that the iEEG coordinate frame is read correctly."""
     bids_path = mne_bids.BIDSPath(
         subject="01",
         session="01",
@@ -11,12 +11,10 @@ def test_read_ieeg_bids_data():
         run="01",
         root=Path(__file__).parent / "data" / "ieeg_bids",
     )
-
     raw_read = mne_bids.read_raw_bids(bids_path)
 
-    assert (
-        raw_read.get_montage().get_positions()["coord_frame"] == "MNI152NLin2009bAsym"
-    )
+    coord_frame = raw_read.get_montage().get_positions()["coord_frame"]
+    assert coord_frame == "MNI152NLin2009bAsym"
 
 
 if __name__ == '__main__':
