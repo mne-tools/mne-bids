ed75468b8209d1fe3d9002163a38c433c70c3ddd
Thomas Hartmann <thomas.hartmann@th-ht.de>
2024-10-11 13:39:01 +0200

    Fix handling of README files with extensions (#1318)
    * we have a failing test!
    
    * lint fixes
    
    * check for existing README with all allowed extensions and use that one.
    
    * updated whats new
    
    * fix scope
    
    * Update doc/whats_new.rst
    
    Co-authored-by: Daniel McCloy <dan@mccloy.info>
    
    * remove extra print command
    
    Co-authored-by: Daniel McCloy <dan@mccloy.info>
    
    * do not cast to str
    
    * throw exception when more than one readme is encountered.
    
    * added a test for multiple readmes
    
    ---------
    
    Co-authored-by: Daniel McCloy <dan@mccloy.info>

--- mne_bids/write.py
+++ mne_bids/write.py
@@ -1955,7 +1955,18 @@ def write_raw_bids(
     )
 
     # For the remaining files, we can use BIDSPath to alter.
-    readme_fname = op.join(bids_path.root, "README")
+    readme_suffixes = ("", ".md", ".rst", ".txt")
+    found_readmes = sorted(
+        filter(lambda x: x.suffix in readme_suffixes, bids_path.root.glob("README*"))
+    )
+    if len(found_readmes) > 1:
+        raise RuntimeError(
+            "Multiple README files found in the BIDS root folder. "
+            "This violates the BIDS specifications. "
+            "Please ensure there is only one README file."
+        )
+    readme_fname = str((found_readmes or [bids_path.root / "README"])[0])
+
     participants_tsv_fname = op.join(bids_path.root, "participants.tsv")
     participants_json_fname = participants_tsv_fname.replace(".tsv", ".json")
 
