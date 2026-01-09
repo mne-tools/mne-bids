5643ce2c1a0b381d27a3f7c79f813d3c2c90f5ad
Thomas S. Binns <t.s.binns@outlook.com>
2026-01-09 09:52:47 +0100

    Remove repeated words

--- examples/anonymize_dataset.py
+++ examples/anonymize_dataset.py
@@ -207,7 +207,7 @@ print_dir_tree(bids_root_anon)
 # culture, like "42", or "1337". To obtain a truly random seed, you can paste
 # the following into your console:
 # ``python -c "import secrets; print(secrets.randbits(31))"``
-# Here, 31 bits correspond to the maximum seed "size" that the the legacy
+# Here, 31 bits correspond to the maximum seed "size" that the legacy
 # ``RandomState`` by NumPy, which many scientific libraries still rely on,
 # can accept. For more information, see also this blog post on
 # `NumPy RNG best practices <https://blog.scientific-python.org/numpy/numpy-rng/>`_.
