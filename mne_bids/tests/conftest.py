7cf096cdab393f7f3e6b92af4050418415451525
Scott Huberty <seh33@uw.edu>
2026-03-15 12:52:32 -0700

    TST: triage tests based on version

--- mne_bids/tests/conftest.py
+++ mne_bids/tests/conftest.py
@@ -4,21 +4,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import platform
+import re
 import shutil
 
 import pytest
 from mne.utils import run_subprocess
+from packaging.version import Version
 
 
 @pytest.fixture(scope="session")
 def _bids_validate():
     """Fixture to run BIDS validator."""
-    # See: https://stackoverflow.com/q/28891053/5201771
-    # On Windows, shell must be True
-    if platform.system() == "Windows":
-        shell = True
-    else:
-        shell = False
+    shell = _use_shell()
 
     # If neither bids-validator nor npx are available, we cannot validate BIDS
     # datasets, so we raise an exception.
@@ -44,6 +41,40 @@ def _bids_validate():
             run_subprocess(cmd, shell=shell)
 
     return _validate
+
+
+@pytest.fixture(scope="session")
+def _validator_version():
+    """Return bids-validator version or None if unknown/uninstalled."""
+    shell = _use_shell()
+    validator_path = shutil.which("bids-validator")
+    npx_path = shutil.which("npx")
+
+    if validator_path is not None:
+        cmd = [validator_path, "--version"]
+    elif npx_path is not None:
+        cmd = [npx_path, "bids-validator@latest", "--version"]
+    else:
+        return None
+
+    out = run_subprocess(cmd, shell=shell)[0]
+    match = re.search(r"\d+\.\d+\.\d+", out)  # MAJOR.MINOR.PATCH
+    if match is None:
+        return None
+    return Version(match.group(0))
+
+
+@pytest.fixture(scope="session")
+def _using_legacy_validator(_validator_version):
+    """Is `bids-validator --version` < 2."""
+    return _validator_version is not None and _validator_version.major < 2
+
+
+def _use_shell():
+    """Whether to run subprocess with shell injection."""
+    # See: https://stackoverflow.com/q/28891053/5201771
+    # On Windows, shell must be True
+    return platform.system() == "Windows"
 
 
 # Deal with:
