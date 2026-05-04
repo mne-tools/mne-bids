f727b134a64efe92b0239cba62ae58fd0a510a01
Bru <b.aristimunha@gmail.com>
2026-05-04 23:38:21 +0200

    FIX: warn instead of raise when subject row missing from participants.tsv (#1606)

--- mne_bids/read.py
+++ mne_bids/read.py
@@ -297,6 +297,10 @@ def _verbose_list_index(lst, val, *, allow_all=False):
 def _handle_participants_reading(participants_fname, raw, subject):
     participants_tsv = _from_tsv(participants_fname)
     subjects = participants_tsv["participant_id"]
+    if subject not in subjects:
+        warn(f"Subject {subject!r} is not listed in {participants_fname.name}")
+        raw.info["subject_info"] = dict()
+        return raw
     row_ind = _verbose_list_index(subjects, subject, allow_all=True)
     raw.info["subject_info"] = dict()  # start from scratch
 
