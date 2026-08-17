d3d872f3dc4ab0551cdb51b0cbfb7b3f02e4da7f
Circle Ci <circle@mne.com>
2026-08-17 22:47:38 +0000

    doc updates [skip ci]

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.20.0.dev28+g1c22b05c6',
+    VERSION: '0.20.0.dev29+g526ee9ddb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
