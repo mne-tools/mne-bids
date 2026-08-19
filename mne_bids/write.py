0406c09275398934ffeb0d16bc44eee4015394ce
nordme <nordme@uw.edu>
2026-08-18 21:47:44 -0700

    split save fix

--- mne_bids/write.py
+++ mne_bids/write.py
@@ -2300,7 +2300,8 @@ def write_raw_bids(
                 raise FileNotFoundError(
                     f"Empty-room data file not found: {aep}"
                 )
-        associated_er_path = check_splits[0].relative_to(bids_path.root)
+        use_path = check_splits[0] if len(check_splits) > 1 else associated_er_path
+        associated_er_path = use_path.relative_to(bids_path.root)
         #associated_er_path = associated_er_path.relative_to(bids_path.root)
         # Ensure it works on Windows too
         associated_er_path = associated_er_path.as_posix()
